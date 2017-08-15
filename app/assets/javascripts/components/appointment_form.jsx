0fc69b231176203a031a3e40494488ebee853f7d
michaelyatco <mjyatco@gmail.com>
2017-08-15 17:03:38 -0400

    AppointmentForm added via React logic

---
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -0,0 +1,14 @@
+var AppointmentForm = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h2>Make a new appointment</h2>
+        <form>
+          <input name="title" placeholder="Appointment Title" />
+          <input name="appt_time" placeholder="Date and Time" />
+          <input name="submit" value="Make Appointment" />
+        </form>
+      </div>
+    )
+  }
+})
