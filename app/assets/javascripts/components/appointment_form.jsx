6bacac90e59b8f56e3ec9a06936efb0d2b099e24
michaelyatco <mjyatco@gmail.com>
2017-08-16 13:27:18 -0400

    onChange method added to allow editable text

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,10 +1,19 @@
 var AppointmentForm = React.createClass({
+  handleChange: function(e) {
+    var name = e.target.name;
+    obj = {};
+    obj[name] = e.target.value;
+    this.props.onUserInput(obj);
+  },
+
   render: function() {
     return (
       <div>
         <h2>Make a new appointment</h2>
         <form>
-          <input name="title" placeholder="Appointment Title" value={this.props.input_title}/>
+          <input name="title" placeholder="Appointment Title"
+            value={this.props.input_title}
+            onChange={this.handleChange}/>
           <input name="appt_time" placeholder="Date and Time" value={this.props.input_appt_time}/>
           <input name="submit" value="Make Appointment" />
         </form>
--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -7,11 +7,16 @@ var Appointments = React.createClass({
     }
   },
 
+  handleUserInput: function(obj) {
+    this.setState(obj);
+  },
+
   render: function() {
     return (
       <div>
         <AppointmentForm input_title={this.state.input_title}
-          input_appt_time={this.state.input_appt_time}/>
+          input_appt_time={this.state.input_appt_time}
+          onUserInput={this.handleUserInput} />
         <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
